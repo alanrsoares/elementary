9fc46e742ff231e149b15aafe2960cde9304ad3f
Alan R. Soares <alanrsoars@gmail.com>
2015-03-22 13:35:07 +1300

    Adds initial components and index.html

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+React.render(<App />, document.getElementById('#app'));
---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from './hello';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <div class="app-container">
+        <Hello name="Juca" />
+      </div>
+    )
+  }
+}

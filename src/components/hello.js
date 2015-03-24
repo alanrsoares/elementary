374a19ed31ca163040954abab33595318815ee80
Alan R. Soares <alanrsoars@gmail.com>
2015-03-24 22:13:58 +1300

    Adds property types to hello component

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 export default class Hello extends React.Component {
+
+  static get propTypes() {
+    return {
+      name: React.PropTypes.string
+    }
+  }
+
   render(){
     return <h1>Hello, {this.props.name}!</h1>
   }
